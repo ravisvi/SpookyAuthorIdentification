565be1081024a2f19e82f7dc99f18eb869106240
amberm291 <ambermadvariya@gmail.com>
2018-12-02 21:14:54 -0500

    Adding new branch

--- src/lang_model_lstm.py
+++ src/lang_model_lstm.py
@@ -1,16 +1,18 @@
 from keras.preprocessing.sequence import pad_sequences
-from keras.layers import Embedding, LSTM, Dense, Dropout
+from keras.layers import Embedding, LSTM, Dense, Dropout, TimeDistributed, Activation
 from keras.preprocessing.text import Tokenizer
-from keras.callbacks import EarlyStopping
+from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras.models import Sequential
 import keras.utils as ku 
 import numpy as np 
 
 class LSTMLanguageModel:
-    def __init__(self):
+    def __init__(self, embedding_size=256, layer_size=256):
         self.tokenizer = Tokenizer()
         self.reverse_index = {}
         self.max_len = -1*float("inf")
+        self.embedding_size = embedding_size
+        self.layer_size = layer_size
 
     def text_to_seq(self, text):
         self.tokenizer.fit_on_texts([text])
@@ -28,24 +30,39 @@ class LSTMLanguageModel:
     def finish_corpus(self):
         self.total_words = len(self.tokenizer.word_index) + 1
 
-    def train_model(self, input_sequences):
-        predictors, label = input_sequences[:,:-1],input_sequences[:,-1]
-        label = ku.to_categorical(label, num_classes=self.total_words)
+    def train_model_generator(self, data_generator, data_path=""):
         self.model = Sequential()
-        self.model.add(Embedding(self.total_words, 10, input_length=self.max_len - 1))
-        self.model.add(LSTM(150, return_sequences=True))
-        self.model.add(Dropout(0.2))
-        self.model.add(LSTM(100))
-        self.model.add(Dense(self.total_words, activation='softmax'))
+        vocabulary = data_generator.vocabulary
+        num_steps = data_generator.num_steps
+        batch_size = data_generator.batch_size
+        self.model.add(Embedding(vocabulary, self.embedding_size, input_length=num_steps))
+        self.model.add(LSTM(self.layer_size, return_sequences=True))
+        self.model.add(LSTM(self.layer_size, return_sequences=True))
+        self.model.add(Dropout(0.5))
+        self.model.add(TimeDistributed(Dense(vocabulary)))
+        self.model.add(Activation('softmax'))
+
+        self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['categorical_accuracy'])
+        checkpointer = ModelCheckpoint(filepath=data_path, verbose=1)
+        num_epochs = 50
+        model.fit_generator(data_generator.generate(), len(train_data)//(batch_size*num_steps), num_epochs, callbacks=[checkpointer])
+        print(self.model.summary())
+
+    def train_model(self, data_object):
+        predictors, label = data_object.get_inputs()
+        total_words = data_object.get_vocabulary_size()
+        label = ku.to_categorical(label, num_classes=total_words)
+        self.model = Sequential()
+        self.model.add(Embedding(total_words, self.embedding_size, input_length=predictors.shape[1]))
+        self.model.add(LSTM(self.layer_size))#, return_sequences=True))
+        self.model.add(Dropout(0.3))
+        self.model.add(Dense(total_words, activation='softmax'))
 
         self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
         earlystop = EarlyStopping(monitor='val_loss', min_delta=0, patience=5, verbose=0, mode='auto')
-        self.model.fit(predictors, label, epochs=1, verbose=1, callbacks=[earlystop], batch_size=512)
+        self.model.fit(predictors, label, epochs=50, verbose=1, callbacks=[earlystop], batch_size=512)
         print(self.model.summary())
 
-    def generate_reverse_index(self):
-        for word, index in self.tokenizer.word_index.items():
-            self.reverse_index[index] = word
 
     def generate_text(self, seed_text, next_words, max_sequence_len):
         for _ in range(next_words):
--- src/run_lm.py
+++ src/run_lm.py
@@ -1,24 +1,60 @@
 import sys
 from lang_model_lstm import LSTMLanguageModel
+import numpy as np
+import string
+
+class DataGenerator(object):
+    def __init__(self, input_size=1):
+        self.input_size = input_size
+        self.word_idx = {}
+        self.reverse_idx = {}
+        self.word_idx["<s>"] = 0
+        self.reverse_idx[0] = "<s>"
+        self.word_idx["</s>"] = 1
+        self.reverse_idx[1] = "</s>"
+        self.count = 2
+        self.input_seq = []
+
+    def parse_text(self, word_list):
+        self.input_seq.append(0)
+        for word in word_list:
+            if word not in self.word_idx:
+                self.word_idx[word] = self.count
+                self.reverse_idx[self.count] = word
+                self.count += 1
+            self.input_seq.append(self.word_idx[word])
+        self.input_seq.append(1)
+
+    def get_inputs(self):
+        input_matrix = self.input_seq[:-1]
+        input_matrix = np.asarray(input_matrix).reshape((len(self.input_seq) - 1,1))
+        targets = self.input_seq[1:]
+        targets = np.asarray(targets).reshape((len(self.input_seq) - 1,1))
+        return input_matrix, targets
+
+    def get_vocabulary_size(self): 
+        return len(self.word_idx)
 
 def main(fname):
-	lm_object = LSTMLanguageModel()
-	infile = open(fname)
-	first = True
-	input_seq = []
-	for line in infile:
-		if first:
-			first = False
-			continue
-		text_id, text, author = line.split('","')
-		if not author.startswith("EAP"): continue
-		input_seq.extend(lm_object.text_to_seq(text.lower()))
-	input_seq = lm_object.get_input(input_seq)
-	print(input_seq.shape)
-	lm_object.finish_corpus()
-	lm_object.generate_reverse_index()
-	lm_object.train_model(input_seq)
+    data_object = DataGenerator()
+    lm_object = LSTMLanguageModel()
+    infile = open(fname)
+    first = True
+    input_seq = []
+    for line in infile:
+        if first:
+            first = False
+            continue
+        text_id, text, author = line.split('","')
+        if not author.startswith("EAP"): continue
+        text = text.translate(None, string.punctuation).lower().split()
+        data_object.parse_text(text)
+    predictors, label = data_object.get_inputs()
+    print(predictors.shape)
+    print(label.shape)
+    print(data_object.get_vocabulary_size())
+    lm_object.train_model(data_object)
 
 
 if __name__=="__main__":
-	main("/Users/ambermadvariya/Documents/585/project/data/train.csv")
+    main("/Users/ambermadvariya/Documents/585/project/data/train.csv")
